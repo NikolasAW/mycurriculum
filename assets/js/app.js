61e8260b5a2eb746a6c70283fa2463800a60063f
NIKOLAS ADRIANO WAHRMEISTER <nikolas.wahrmeister@alunos.sc.senac.br>
2021-06-24 15:34:30 -0300

    criado o site darkmode

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var modoNoturno = function () {
+    var body = document.getElementById('corpo');
+    if (body.classList.contains('modo-noturno')) {
+        body.classList.remove('modo-noturno');
+    } else {
+        body.classList.add('modo-noturno');
+    }
+}
